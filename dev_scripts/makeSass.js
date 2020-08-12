7de0000c8d8a74c4290f69b576f22c5ee7173adb
Steph <fillon.st@gmail.com>
2020-08-12 16:15:55 +0200

    maj2
    Import Sass

---
+++ dev_scripts/makeSass.js
@@ -0,0 +1,20 @@
+console.clear();
+var sass = require('sass');
+var fs = require('fs');
+ 
+const src=[
+	"accueil",
+	"menus",
+	"mentions",
+];
+
+let result = "";
+for( let i=0; i<src.length; i++){	
+	result += sass.renderSync({file: "./scss/"+src[i]+".scss"}).css;
+}
+ 
+fs.writeFile("./css/result.css", result, function(err){
+        if(!err){
+          console.log("ok")
+        }
+      });
